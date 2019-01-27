b8b710100c0af9791de97cc069a9e6fc4bad952d
mhe <mail@marcus-herrmann.com>
2019-01-27 19:01:11 +0100

    Rename component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import MenuItem from "./components/MenuItem.vue";
 import MenuLink from "./components/MenuLink.vue";
-import MenuWrapper from "./components/MenuButton.vue";
+import MenuWrapper from "./components/MenuWrapper.vue";
 
 export { MenuWrapper, MenuItem, MenuLink };
